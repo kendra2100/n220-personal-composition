99485e0ac5995221080ad555f6004a3eb5411d05
Kendra Sipes <kendrasipes@Kendras-MacBook-Pro.local>
2020-09-03 23:17:44 -0400

    Init

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(720, 400);
+}
+
+function draw() {
+  background(220);
+
+  fill(26, 240, 89, 140);
+  stroke(49, 459, 35, 393);
+
+  ellipse(50, 50, 80, 80);
+
+  ellipse(150, 150, 80, 80);
+
+  ellipse(70, 300, 80, 80);
+
+  ellipse(290, 423, 80, 80);
+
+  ellipse(341, 341, 341, 341);
+}
