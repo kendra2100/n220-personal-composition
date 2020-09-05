fed026cdc1e0d9851dd9bdffe303a5d979963fe4
Kendra Sipes <kendrasipes@Kendras-MacBook-Pro.local>
2020-09-04 22:14:22 -0400

    color, shapes, positions

--- main.js
+++ main.js
@@ -3,18 +3,47 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background("black");
 
-  fill(26, 240, 89, 140);
+  let c = color("magenta");
+  let width = 80;
+
+  fill(c);
   stroke(49, 459, 35, 393);
 
-  ellipse(60, 50, 80, 80);
+  circle(50, 50, width);
 
-  ellipse(150, 150, 80, 80);
+  c = color("#8ee154");
+  fill(c);
+  circle(150, 150, width);
 
-  ellipse(70, 300, 80, 80);
+  c = color("#86d510");
+  fill(c);
+  circle(70, 300, width);
 
-  ellipse(290, 423, 80, 80);
+  c = color("#21885f");
+  fill(c);
+  circle(290, 423, width);
 
-  ellipse(341, 341, 341, 341);
+  c = color("#5ad4a4");
+  fill(c);
+  circle(341, 341, 341);
+
+  c = color("#98605c");
+  fill(c);
+  circle(500, 150, width);
+
+  c = color("#b48f57");
+  fill(c);
+  circle(620, 50, width);
+
+  c = color("#514fdd");
+  fill(c);
+  circle(600, 300, width);
+
+  c = color("yellow");
+  fill(c);
+  circle(340, 50, width);
+
+  triangle(10, 60, 50, 10, 90, 60);
 }
